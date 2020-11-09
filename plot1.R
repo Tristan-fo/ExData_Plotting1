12a1e8bb00225a8afe91746187a1f93654d2b63e
Tristan Foulard <tristan.foulard@gmail.com>
2020-11-09 20:39:18 +0100

    Development of the code to create Plot1

---
+++ plot1.R
@@ -0,0 +1,20 @@
+path <- getwd()
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(url, file.path(path, "data.zip"))
+
+unzip("data.zip")
+
+data <- read.table("household_power_consumption.txt", header = TRUE,
+                   sep = ";", stringsAsFactors = FALSE, 
+                   dec = ".")
+head(data)
+
+data_subset <- subset(data, data$Date %in% c("1/2/2007", "2/2/2007"))
+
+GlobalActivePower <- as.numeric(data_subset$Global_active_power)
+hist(GlobalActivePower, main = "Global Active Power", 
+     col = "red",
+     xlab = "Global Active Power (kilowatts)")
+
+dev.copy(png, "plot1.png", width = 480, height = 480)
+dev.off()
